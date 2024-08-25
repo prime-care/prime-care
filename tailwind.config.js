1b3ff1948e49a57878a39fe2ede12dfeb32747f3
Moamed Megahed <moamenmegahed22@gmail.com>
2024-08-25 15:08:20 +0300

    build: Install tailwind and flowbite react libraries

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+const flowbite = require("flowbite-react/tailwind");
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    flowbite.content(), // flowbite
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    flowbite.plugin() // flowbite
+  ],
+}
