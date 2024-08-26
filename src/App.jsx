146aa71be7037eed97edfd11a4b4f22f312b26d4
Mohamed Omar <mohamed.omar.mubark@gmail.com>
2024-08-27 00:53:39 +0300

    feat: Prepare products page route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // pages and components imports
 import Header from "./modules/common/components/Header";
 import Home from "./modules/user/pages/Home";
+import Products from "./modules/user/pages/Products";
 
 // react router
 import { Route, Routes } from "react-router-dom";
@@ -11,6 +12,7 @@ const App = () => {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
       </Routes>
     </>
   );
