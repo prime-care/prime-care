66c8302e0a26d5148f2a78680a4434f5f4b34eee
belal_soliman <belalsoliman8@gmail.com>
2024-08-27 01:35:14 +0300

    feat: add my account page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // pages and components imports
 import Header from "./modules/common/components/Header";
 import Home from "./modules/user/pages/Home";
+import MyAccount from "./modules/user/pages/MyAccount";
 import Products from "./modules/user/pages/Products";
 
 // react router
@@ -13,6 +14,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
+        <Route path="/my-account" element={<MyAccount />} />
       </Routes>
     </>
   );
