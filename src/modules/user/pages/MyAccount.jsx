1da98bf6e928f3a640f557f25d1750b7c277bd44
Moamed Megahed <moamenmegahed22@gmail.com>
2024-08-27 22:24:27 +0300

    refactor: Make MainTitle component reusable and update its styling

--- src/modules/user/pages/MyAccount.jsx
+++ src/modules/user/pages/MyAccount.jsx
@@ -1,10 +1,10 @@
-import Account from "../components/my-account/Account";
+import MainTitle from "../components/my-account/MainTitle";
 import OrderHistory from "../components/my-account/OrderHistory";
 import Dashboard from "../components/my-account/Dashboard";
 const MyAccount = () => {
   return (
     <>
-      <Account />
+      <MainTitle title={"My Account"} />
 
       <div className="mt-9 ">
         <OrderHistory />
