a070547a5adcce2f4cb461947f4e1e752fdc50cc
Mohamed Omar <mohamed.omar.mubark@gmail.com>
2024-09-03 15:44:51 +0300

    feat: :sparkles: Add ProductCard reusable component

---
+++ src/modules/common/components/ProductCard.jsx
@@ -0,0 +1,54 @@
+// icons
+import { IoCartOutline } from "react-icons/io5";
+import { FaRegHeart } from "react-icons/fa";
+import { Rating } from "flowbite-react";
+
+export default function ProductsCard({ product }) {
+  return (
+    <div className="single-product p-3 border border-gray-300 rounded-md flex flex-col gap-1 cursor-pointer">
+      <div className="product-image mb-2 relative h-52">
+        <img
+          src={product?.image}
+          alt={product?.name}
+          title={product?.name}
+          className="rounded-md h-full w-full object-contain"
+          onClick={() => navigateToProduct(product?.productId)}
+        />
+
+        <div className="product-actions absolute w-1/4 h-full top-0 bg-primary rounded-r-md overflow-hidden">
+          <div
+            onClick={() => {
+              addToCart(product?.id);
+            }}
+            className="add-to-cart flex justify-center items-center h-1/2 cursor-pointer transition-all duration-300">
+            <IoCartOutline className="text-2xl text-white" />
+          </div>
+
+          <div
+            onClick={() => {
+              addToWishlist(product?.id);
+            }}
+            className="add-to-wishlist flex justify-center items-center h-1/2 cursor-pointer transition-all duration-300">
+            <FaRegHeart className="text-2xl text-white" />
+          </div>
+        </div>
+      </div>
+      <span className="text-sm font-medium text-gray-500">
+        {product?.categoryName}
+      </span>
+      <h3 className="text-base font-semibold text-primary truncate overflow-hidden whitespace-nowrap">
+        {product?.name}
+      </h3>
+      <Rating>
+        <Rating.Star />
+        <Rating.Star />
+        <Rating.Star />
+        <Rating.Star />
+        <Rating.Star filled={false} />
+      </Rating>
+      <span className="text-base font-bold text-primary">
+        {product?.price} <span className="text-sm text-gray-500">USD</span>
+      </span>
+    </div>
+  );
+}
--- src/modules/user/components/products/ProductsList.jsx
+++ src/modules/user/components/products/ProductsList.jsx
@@ -4,9 +4,7 @@ import { useState, useEffect } from "react";
 import Search from "../../../common/components/Search";
 import PaginationComponent from "../../../common/components/Pagination";
 import { Spinner } from "flowbite-react";
-// icons
-import { IoCartOutline } from "react-icons/io5";
-import { FaRegHeart } from "react-icons/fa";
+import ProductCard from "../../../common/components/ProductCard";
 
 // navigate
 import { useNavigate } from "react-router-dom";
@@ -43,56 +41,7 @@ const ProductsList = ({ products, loading }) => {
       ) : (
         <div className="products gap-3 my-4">
           {products.map((product) => (
-            <div
-              key={product.id}
-              className="single-product p-3 border border-gray-300 rounded-md flex flex-col gap-1 cursor-pointer"
-            >
-              <div className="product-image mb-2 relative h-52">
-                <img
-                  src={product.image}
-                  alt={product.name}
-                  title={product.name}
-                  className="rounded-md h-full w-full object-contain"
-                  onClick={() => navigateToProduct(product.productId)}
-                />
-
-                <div className="product-actions absolute w-1/4 h-full top-0 bg-primary rounded-r-md overflow-hidden">
-                  <div
-                    onClick={() => {
-                      addToCart(product.id);
-                    }}
-                    className="add-to-cart flex justify-center items-center h-1/2 cursor-pointer transition-all duration-300"
-                  >
-                    <IoCartOutline className="text-2xl text-white" />
-                  </div>
-
-                  <div
-                    onClick={() => {
-                      addToWishlist(product.id);
-                    }}
-                    className="add-to-wishlist flex justify-center items-center h-1/2 cursor-pointer transition-all duration-300"
-                  >
-                    <FaRegHeart className="text-2xl text-white" />
-                  </div>
-                </div>
-              </div>
-              <h3
-                className="text-sm font-medium text-gray-500"
-                onClick={() => navigateToProduct(product.productId)}
-              >
-                {product.categoryName}
-              </h3>
-              <h3
-                className="text-base font-medium text-gray-700"
-                onClick={() => navigateToProduct(product.productId)}
-              >
-                {product.name}
-              </h3>
-              <span className="text-base font-bold text-primary">
-                {product.price}{" "}
-                <span className="text-sm text-gray-500">USD</span>
-              </span>
-            </div>
+            <ProductCard key={product.id} product={product} />
           ))}
         </div>
       )}
