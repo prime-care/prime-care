9e8d910d2a3ccc3c8c0857132a99283ab9fe2c9b
Moamed Megahed <moamenmegahed22@gmail.com>
2024-09-03 20:09:22 +0300

    feat: Set up Redux and implement cart operations (add, remove, update quantity)

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
+// redux
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+
 // react router
 import { BrowserRouter } from "react-router-dom";
 
@@ -11,8 +15,10 @@ import "./styles/pages/products.scss";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>
 );
---
+++ src/redux/slices/cartSlice.js
@@ -0,0 +1,46 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState: {
+        items: [],
+    },
+    reducers: {
+        addToCart: (state, action) => {
+            const { id, name, image, price, quantity } = action.payload;// {id:p1, name:Cerum, image:"url", price:20}
+            const existingItem = state.items.find(item => item.id === id);
+
+            if (existingItem) {
+                // Update quantity if item already exists in cart
+                existingItem.quantity += quantity;
+            } else {
+                // Add new item to cart
+                state.items.push({ id, name, image, price, quantity });
+            }
+        },
+        adjustQuantity: (state, action) => {
+            const { id, quantity } = action.payload;
+            const item = state.items.find(item => item.id === id);
+
+            if (item) {
+                item.quantity += quantity;
+
+                // Remove item if quantity is less than or equal to zero
+                if (item.quantity <= 0) {
+                    state.items = state.items.filter(item => item.id !== id);
+
+                }
+            }
+        },
+        removeFromCart: (state, action) => {
+            const id = action.payload;
+            state.items = state.items.filter(item => item.id !== id);
+        },
+
+    },
+});
+
+
+
+export const { addToCart, adjustQuantity, removeFromCart } = cartSlice.actions;
+export default cartSlice.reducer;
