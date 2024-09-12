b333f6f2ca594fc9d5235ec380dcb0852bbfff64
Mohamed Omar <mohamed.omar.mubark@gmail.com>
2024-09-12 16:06:49 +0300

    refactor: :recycle: User wishlist refactoring

--- src/modules/user/components/my-account/WishList.jsx
+++ src/modules/user/components/my-account/WishList.jsx
@@ -1,58 +1,65 @@
+import { useState } from "react";
+
 // components
-import { Table } from "flowbite-react";
-import { Button } from "flowbite-react";
-import { IoIosHeartDislike } from "react-icons/io";
+import { Spinner } from "flowbite-react";
+import ProductCard from "../../../common/components/ProductCard";
 
 const WishList = () => {
+  const [loading, setLoading] = useState(false);
+
   const products = [
     {
       id: 1,
       name: "Product 1",
       price: 100,
     },
+    {
+      id: 2,
+      name: "Product 2",
+      price: 200,
+    },
+    {
+      id: 3,
+      name: "Product 3",
+      price: 300,
+    },
+    {
+      id: 4,
+      name: "Product 4",
+      price: 400,
+    },
+    {
+      id: 5,
+      name: "Product 5",
+      price: 500,
+    },
   ];
 
-  const removeFromWishlist = (id) => {
-    console.log(id);
-  };
-
   return (
-    <div className="overflow-x-auto">
+    <div>
       <div className="head">
         <h1 className="mb-6 text-2xl font-medium text-gray-700">Wishlist</h1>
       </div>
 
-      <Table>
-        <Table.Head>
-          <Table.HeadCell>Product</Table.HeadCell>
-          <Table.HeadCell>Price</Table.HeadCell>
-          <Table.HeadCell></Table.HeadCell>
-        </Table.Head>
-        <Table.Body className="divide-y">
+      {loading ? (
+        <div className="loading text-center my-4 text-xl font-semibold">
+          <Spinner aria-label="Extra large spinner example" size="xl" />
+        </div>
+      ) : products.length === 0 ? (
+        <div className="py-10 mt-4 bg-secondaryBg rounded-lg">
+          <p className="no-data text-center text-xl font-semibold text-gray-700">
+            No Products Found
+          </p>
+        </div>
+      ) : (
+        <div
+          className="products grid gap-4 
+          grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           {products.map((product) => (
-            <Table.Row
-              key={product.id}
-              className="bg-white dark:border-gray-700 dark:bg-gray-800">
-              <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                {product.name}
-              </Table.Cell>
-              <Table.Cell>
-                {product.price + " "}
-                <span>EGP</span>
-              </Table.Cell>
-              <Table.Cell>
-                <div className="flex gap-3">
-                  <Button
-                    color="failure"
-                    onClick={() => removeFromWishlist(product.id)}>
-                    <IoIosHeartDislike />
-                  </Button>
-                </div>
-              </Table.Cell>
-            </Table.Row>
+            <ProductCard key={product.id} product={product} />
           ))}
-        </Table.Body>
-      </Table>
+        </div>
+      )}
     </div>
   );
 };
